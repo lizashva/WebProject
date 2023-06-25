738fcec04370a9603640d330dd34421f9448b351
lizashva <lizashva556@gmail.com>
2023-06-25 12:20:46 +0300

    vite

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
-
+console.log("sharon")
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact()],
